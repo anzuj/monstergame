b4659ab7048c0657b050962d3ad3b445dee41bd5
Anzelika Häberli <anzelika@joineer.ch>
2021-01-25 15:25:32 +0100

    update link

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
         </v-card>
         <p class="text-center text-caption" v-if="$route.name ==='fight'">
           ..or just have some fun with my
-          <a href="./random-loot">loot generator</a>
+          <a @click="$router.push({name: 'loot'})">loot generator</a>
         </p>
           <p class="text-center text-caption" v-if="$route.name ==='loot'">
           ..or work for the loot in my
-          <a href="./fight">lil' monstergame</a>
+          <a @click="$router.push({name: 'fight'})">lil' monstergame</a>
         </p>
 
         <v-footer app dark>
